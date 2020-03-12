f2b5d58e5ef28c6cc977378b13141123e80dc710
zgard <zfgardner@gmail.com>
2020-03-12 18:32:12 -0400

    Almost finished with Part 2 of Scene-it

---
+++ index.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded',function(){
+    
+    function renderMovies(movieArray) {
+        var moviesHTML = movieArray.map(function(currentMovie){
+            return `
+            <div class="movie card col-2" style="width: 18rem; padding: 10px; margin: 5px; flex-wrap: wrap;">
+            <img class="image card-img-top" src="${currentMovie.Poster}"/>
+						<div class="card-body">
+							<h1 class="movie-title card-title" style="width: 100%;">${currentMovie.Title}</h1>
+							<h2 class="movie-release-date card-text">${currentMovie.Year}</h2>
+							<button class="addButton" onclick="saveToWatchlist('${currentMovie.imdbID}')">Add</button>
+                        </div>
+            </div>
+            `
+        }).join('');
+    return moviesHTML;
+    }
+    var moviesContainer = document.getElementById('movies-container');
+
+    moviesContainer.innerHTML = renderMovies(movieData);
+
+    document.getElementById("input").addEventListener("input", function(e){
+        e.preventDefault()
+        var searchString = e.target.value.toLowerCase();
+		var filteredData = movieData.filter(function (movie) {
+			var foundInTitle = movie.Title.toLowerCase().indexOf(searchString) > -1;
+			var foundInDate = movie.Year.toLowerCase().indexOf(searchString) > -1;
+			return foundInTitle || foundInDate;
+        });
+        document.getElementById('movies-container').innerHTML = renderMovies(filteredData);
+    });
+    
+});
+
+function saveToWatchlist(imdbID){
+    var movie = movieData.find(function(currentMovie){
+        return currentMovie.imdbID == imdbID;
+    });
+    var watchlistJSON = localStorage.getItem("watchlist");
+    var watchlist = JSON.parse(watchlistJSON);
+    if (watchlistJSON == null){
+        watchlist = [];
+    }
+        watchlist.push(movie);
+        watchlistJSON = JSON.stringify(watchlist);
+        localStorage.setItem("watchlist", watchlistJSON);
+    }
